487f54b1abd10a0725bcfaa82a2061a27851ddaf
kcondic <kreso@dump.hr>
2020-11-16 00:54:24 +0100

    Made changes which will conflict with tortoisegit branch

--- DUMPInternship2020Git/DUMP/Program.cs
+++ DUMPInternship2020Git/DUMP/Program.cs
@@ -7,8 +7,9 @@ namespace DUMP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Conflicting change in branch for rebase");
+            Console.WriteLine("Conflicting change in branch for rebase - changed again for tortoisegit");
             Console.WriteLine("Hello from third commit");
+            Console.WriteLine("Tortoisegit new line in Program.cs");
         }
     }
 }
