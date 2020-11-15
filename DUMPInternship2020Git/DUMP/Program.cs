e24a4157632eaad47f9d92c2c58b85b0d6c73c17
kcondic <kreso@dump.hr>
2020-11-15 19:00:21 +0100

    Added initial project architecture

---
+++ DUMPInternship2020Git/DUMP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DUMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
